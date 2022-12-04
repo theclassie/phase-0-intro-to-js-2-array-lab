f8efaa3f7c86955cf6ed673246dc53cf454cc355
Tristyn Broadhead <trisclassie@gmail.com>
2022-12-03 18:14:27 -0700

    commit tree 5696a4c54289d2c199babcca7508b085010cb19e

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
-// Write your solution here!
+// // Write your solution here!
+// const cats = ["Milo", "Otis", "Garfield"];
+
+// function destructivelyAppendCat(name) {
+//   cats.push(name);
+// }
+// function destructivelyPrependCat(name) {
+//   cats.unshift(name);
+// }
+// function destructivelyRemoveLastCat(name) {
+//   cats.pop(name);
+// }
+// function destructivelyRemoveFirstCat(name) {
+//   cats.shift(name);
+// }
+// function appendCat(name) {
+//   const newCatsArray = [...cats, name];
+//   return newCatsArray;
+// }
+// function prependCat(name) {
+//   const newCatsArray = [name, ...cats];
+//   return newCatsArray;
+// }
+// function removeLastCat() {
+//   const newRemoveLastCatArray = cats.slice(0, -1);
+//   return newRemoveLastCatArray;
+// }
+// function removeFirstCat() {
+//   const newRemoveFirstCatArray = cats.slice(1);
+//   return newRemoveFirstCatArray;
+// }
+
 const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(name) {
+function destructivelyAppendCat(name){
   cats.push(name);
-}
-function destructivelyPrependCat(name) {
+};
+
+function destructivelyPrependCat(name){
   cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat(){
+  cats.pop();
+};
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+};
+
+function appendCat(name){
+  const newCats = [...cats, name]
+  return newCats;
 }
-function destructivelyRemoveLastCat(name) {
-  cats.pop(name);
+
+function prependCat(name){
+  const newCats2 = [name, ...cats]
+  return newCats2;
 }
-function destructivelyRemoveFirstCat(name) {
-  cats.shift(name);
+
+function removeLastCat(){
+  const byeLastCat = cats.slice(0, -1)
+  return byeLastCat;
 }
-function appendCat(name) {
-  const newCatsArray = [...cats, name];
-  return newCatsArray;
-}
-function prependCat(name) {
-  const newCatsArray = [name, ...cats];
-  return newCatsArray;
-}
-function removeLastCat() {
-  const newRemoveLastCatArray = cats.slice(0, -1);
-  return newRemoveLastCatArray;
-}
-function removeFirstCat() {
-  const newRemoveFirstCatArray = cats.slice(1);
-  return newRemoveFirstCatArray;
+
+function removeFirstCat(){
+  const byeFirstCat = cats.slice(1)
+  return byeFirstCat;
 }
